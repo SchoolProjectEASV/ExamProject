85569b73f84a1edde633dd7dfa06e9318aea40d4
Matty <Mathiaskrarupmadsen@gmail.com>
2024-05-22 16:30:23 +0200

    test start up

---
+++ Test/ComponentTest/ProductComponentTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Moq;
+using ProductApplication.Interfaces;
+using WireMock.Server;
+
+namespace ProductService.UnitTest.ComponentTest;
+
+/// <summary>
+/// Class containing the component test for the product service
+/// </summary>
+public class ProductComponentTest
+{
+    private WireMockServer _categoryServiceMock;
+    private Mock<IConfiguration> _mockConfiguration;
+    private Mock<IProductService> _mockProductService;
+}
