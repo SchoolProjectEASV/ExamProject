52e283a2d6029147cd08666dc870a2c5a3813d37
JensIssa <71692396+JensIssa@users.noreply.github.com>
2024-06-04 03:09:24 +0200

    Removed token from AuthDB, so it doesn't get stored in the database.

--- projects/Auth/AuthInfrastructure/IAuthRepo.cs
+++ projects/Auth/AuthInfrastructure/IAuthRepo.cs
@@ -3,7 +3,5 @@
 public interface IAuthRepo
 {
     Login AddLogin(Login login);
-    void AddTokenToLogin(Token token);
-    Login GetUserByToken(string token);
     Login GetUsersByUsername(string username);
 }
